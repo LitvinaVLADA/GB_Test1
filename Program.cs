538937ac67807eea5b66b772990f1733f6de2b7c
LitvinaVlada <vladamay@outlook.com>
2023-10-22 17:45:00 +0300

    metod_FilterArray

--- Program.cs
+++ Program.cs
@@ -3,3 +3,30 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string[] FilterArray(string[] inputArray) //метод для фильтра значений массива по длине строки
+    {
+        int count = 0;
+
+        // Подсчет количества строк, длина которых меньше или равна 3 символам
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+                count++;
+        }
+
+        // Создание результирующего массива
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        // Заполнение результирующего массива
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
